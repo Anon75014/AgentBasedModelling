61aa076bbaa9f3ac9fdf126a38233124fa9793f5
Cgolling <71523577+Cgolling@users.noreply.github.com>
2021-10-23 15:14:38 +0200

    Update Mapsimulation

--- python/main.py
+++ python/main.py
@@ -1,7 +1,23 @@
+#%%
+
+# info: these next two lines are useful when editing the sim_map file.
+#       Just do  "Run Cell" and jupyter auto-reloads the sim_map file and generates the map with the updated code... :)
+#       -> Maybe make them to comments when running this file without Jupyter Notebook...
+%reload_ext autoreload
+%autoreload 2
+
+
 import numpy as np
 import matplotlib.pyplot as plt
 from sim_map import Map
 
 if __name__ == '__main__':
-    m = Map()
+    ## Version 0 map:
+    #m = Map(2,1,0,0)
+    
+    ## Version 1 map:
+    m = Map(13,1,2,3)
+
+    m.generateMap()
     m.show()
+
--- python/sim_map.py
+++ python/sim_map.py
@@ -2,8 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from itertools import product
 from matplotlib import cm
-from matplotlib.colors import ListedColormap
-
+import matplotlib.colors as colors
+from math import ceil
 class Catchment:
     """
     Catchments in which the farmers will compete
@@ -15,24 +15,59 @@ class Map:
     """
     Entire map, contains all the catchments
     """
-    def __init__(self):
-        pass
+    def __init__(self, N,low,medium,high):
+        """
+        Initialise the map matrix 'm'. And store colour values.
+
+        Input:
+            N = amount of lines of water
+            high, medium, low = amount of water rows with that water-intensity
+        
+        """
+        water = N - 2*high - 2*medium - 2*low
+        self.m = np.ones((N, N-water))
+
+        self.amounts = [low,medium,high,water]
+        self.colours = ["#d16654","#bf9952", "#71B48D","#4170ba"] # (left to right: red, orange, green, blue)
+        self.weights = [0.25,0.5, 1, 10,11]
+
+        print('Done: Initialised map.')
+
+    def generateMap(self):
+        """
+        Generate the Map entries based on the specified amounts of Water.
+        """
+        row = 0
+
+        for index, amount in enumerate(self.amounts): # colour each row
+            for _ in range(amount):
+                self.m[row,:] = self.weights[index]
+                self.m[-(row+1),:] = self.weights[index] # based on symmetry color two at once
+                row += 1
+        
+        print(self.m)    
+        print('Done: Generated map.')
+
+        # Alternatively
+        """ 
+        for i, j in product(range(N), repeat=2):
+            if 0.5 + 1 / N > (i / N) > 0.5 - 1 / N:
+                m[i, j] = 0.0
+            if 0.75 <= (i / N) or (i / N) <= 0.25:
+                m[i, j] = 0.5 
+        """
 
     def show(self):
         """
         Show the map
         """
-        # First experiment with drawing a simple map
-        cmap = ListedColormap(["#7EBCE6", "#DBBEA1", "#71B48D"])
-        N = 10
-        m = np.ones((10, 10))
-        for i, j in product(range(N), repeat=2):
-            if 0.5 + 1 / N > (i / N) > 0.5 - 1 / N:
-                m[i, j] = 0.0
-            if 0.75 <= (i / N) or (i / N) <= 0.25:
-                m[i, j] = 0.5
+        pass
+        
+        
+        # Generate Matplotlib figure
         plt.figure()
-        plt.pcolormesh(m, cmap=cmap, edgecolors='k')
+        crop_cmap, crop_norm = colors.from_levels_and_colors(self.weights, self.colours)
+        plt.pcolormesh(self.m, cmap=crop_cmap, norm=crop_norm, edgecolors='k')
         ax = plt.gca()
         ax.set_aspect('equal')
         plt.show()
