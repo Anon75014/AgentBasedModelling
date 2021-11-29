910fe5cbaf347ef75b69ec1509e60dae7dbd4ac6
Gaël <gael.mourouga@gmail.com>
2021-11-29 18:48:55 +0100

    Updated agents.py with buy_threash=1
    Stopped agents from moving to get v11 from v12

--- Python/agents.py
+++ Python/agents.py
@@ -47,7 +47,7 @@ class Farmer(ap.Agent):
             pos_init = self.random.choice(self.model.unoccupied)
 
         self.model.unoccupied.remove(pos_init)
-        self.buy_cell_threash = self.random.uniform(0, 1)
+        self.buy_cell_threash = 1 #self.random.uniform(0, 1)
 
         """ Set start crop id"""
         crop_id_init = self.random.randint(
